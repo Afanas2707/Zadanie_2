02b71e38950e95a3918aa7297ad3b031ee7e9c3b
Afanas2707 <127185135+Afanas2707@users.noreply.github.com>
2023-12-09 15:43:32 +0300

    Update math_functions.cpp

--- math_functions.cpp
+++ math_functions.cpp
@@ -13,11 +13,5 @@ double multiply(double a, double b) {
 }
 
 double divide(double a, double b) {
-    if (b != 0) {
-        return a / b;
-    }
-    else {
-        // обработка деления на ноль
-        return 0;
-    }
+    return a / b;
 }

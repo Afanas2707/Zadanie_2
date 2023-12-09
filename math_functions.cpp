82ac93c76bf1a477f0f1ad79fd2cf76e508303a4
Afanas2707 <127185135+Afanas2707@users.noreply.github.com>
2023-12-09 15:23:04 +0300

    Create math_functions.cpp

---
+++ math_functions.cpp
@@ -0,0 +1,23 @@
+#include "math_functions.h"
+
+double add(double a, double b) {
+    return a + b;
+}
+
+double subtract(double a, double b) {
+    return a - b;
+}
+
+double multiply(double a, double b) {
+    return a * b;
+}
+
+double divide(double a, double b) {
+    if (b != 0) {
+        return a / b;
+    }
+    else {
+        // обработка деления на ноль
+        return 0;
+    }
+}
